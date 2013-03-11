d8e547ee484a1cb7a2530d135c55a88f6910b3af
Isak Andersson <BitPuffin@lavabit.com>
2013-03-11 14:01:40 +0100

    enforce window creation was successful

--- src/two/core/window.d
+++ src/two/core/window.d
@@ -1,11 +1,15 @@
 module two.core.window;
 
+import std.exception: enforce;
+import allegro5.display;
+
 class Window {
     private:
+        ALLEGRO_DISPLAY* window;
 
     public:
-
-    protected:
-
-
+        this(int width, int height) {
+            window = al_create_display(width, height);
+            enforce(window, "Failed to create display");
+        }
 }
