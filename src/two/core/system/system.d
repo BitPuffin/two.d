df2e46c6be5346d3d5a416ab3955d1f1fc10e335
Isak Andersson <BitPuffin@lavabit.com>
2013-03-11 13:44:10 +0100

    init allegro automatically

--- src/two/core/system/system.d
+++ src/two/core/system/system.d
@@ -1,10 +1,14 @@
 module two.base.system.system;
 
 private {
-    import allegro5.system: al_run_allegro;
+    import allegro5.system: al_run_allegro, al_init;
 }
 
 public:
 
 alias al_run_allegro runTwo;
+
+static this() {
+    al_init();
+}
 
