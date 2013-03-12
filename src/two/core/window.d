d4020b077cd250923e78c550dbe6a9ae912f1010
Isak Andersson <BitPuffin@lavabit.com>
2013-03-12 14:39:24 +0100

    implement flip method

--- src/two/core/window.d
+++ src/two/core/window.d
@@ -3,6 +3,9 @@ module two.core.window;
 import std.exception: enforce;
 import allegro5.display;
 
+// The currently active window in the allegro library
+private ALLEGRO_DISPLAY* current_window;
+
 class Window {
     private:
         ALLEGRO_DISPLAY* window;
@@ -18,12 +21,16 @@ class Window {
             // Create window and throw exception if window is null
             window = al_create_display(width, height);
             enforce(window, "Failed to create display");
+            current_window = window;
 
             // Reset to default after creation
             if (vsync) {
                 al_set_new_display_option(ALLEGRO_DISPLAY_OPTIONS.ALLEGRO_VSYNC, 0, ALLEGRO_SUGGEST);
             }
         }
+    void setTitle(string title) {
+        al_set_window_title(window, title.idup.ptr);
+    }
 
     @property int width() {
         return al_get_display_width(window);
@@ -33,7 +40,15 @@ class Window {
     }
     unittest { auto w = new Window(50, 70); assert(w.width == 50 && w.height == 70, "window resolution properties are broken!"); }
 
-    void setTitle(string title) {
-        al_set_window_title(window, title.idup.ptr);
+    void destroy() {
+        al_destroy_display(window);
+    }
+
+    void flip() {
+        if (window != current_window) {
+            al_set_target_backbuffer(window);
+            current_window = window;
+        }
+        al_flip_display();
     }
 }
