09a5db2648b18db728e85fc46c1cd8096d2ca60e
Isak Andersson <BitPuffin@lavabit.com>
2013-03-13 13:57:17 +0100

    add access to the raw display objects for use with allegro in uncommon circumstances

--- src/two/core/window.d
+++ src/two/core/window.d
@@ -28,32 +28,55 @@ class Window {
                 al_set_new_display_option(ALLEGRO_DISPLAY_OPTIONS.ALLEGRO_VSYNC, 0, ALLEGRO_SUGGEST);
             }
         }
+        /// Not used very often, only if you want to manually construct a window using Allegro's API and then put it in an OO wrapper. Might be a good idea to set your ALLEGRO_DISPLAY pointer to null after calling constructor
+        this(ALLEGRO_DISPLAY* w) {
+            window = w;
+        }
         ~this() {
             al_destroy_display(window);
         }
 
-
+        /// Sets the passed string as the title of the game window
         void setTitle(string title) {
             al_set_window_title(window, title.idup.ptr);
         }
 
+        /// Gets the current width of the game window
         @property int width() {
             return al_get_display_width(window);
         }
+        /// Gets the current hegiht of the game window
         @property int height() {
             return al_get_display_height(window);
         }
         unittest { auto w = new Window(50, 70); assert(w.width == 50 && w.height == 70, "window resolution properties are broken!"); }
 
+        /// Destroys the game window
         void destroy() {
             al_destroy_display(window);
         }
 
+        /** Flips the buffers of the display object
+          *
+          * When draw operations are called on a window
+          * they don't appear on the screen until the buffers
+          * are flipped since all draw operations happen on the
+          * back buffer by default.
+          */
         void flip() {
             if (window != current_window) {
                 al_set_target_backbuffer(window);
                 current_window = window;
             }
             al_flip_display();
+        }
+
+        /** Gets the raw pointer to the ALLEGRO_DISPLAY object.
+         *
+         *  Not used very often, only if you want to complete actions on the raw display object using the allegro API.
+         *  Might be a good idea to set the pointer to null after you are done. Just for the sake of GC.
+         */
+        ALLEGRO_DISPLAY* getAllegroDisplayPointer() {
+            return window;
         }
 }
